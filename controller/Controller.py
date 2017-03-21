5aa322906c264d7a6e77ed29f89c7f945784ef6b
pablobuenaposada <pablo.buenaposada@gmail.com>
2017-03-21 16:08:02 +0100

    very simple main

--- controller/Controller.py
+++ controller/Controller.py
@@ -5,61 +5,12 @@ import locale
 
 
 class Controller:
-    def __init__(self):
-        self.startRecord = -1
-        self.endRecord = -1
-        self.timer = Time()
-        self.fuelCounter = 0
-
     locale.setlocale(locale.LC_ALL, 'en_GB.utf8')
 
+    def __init__(self):
+        self.timer = Time()
 
-def adc2fuel(adc):
-    volts = (adc / 4096.000) * 4.80
-    return int(-7.348540077 * pow(10, -1) * pow(volts, 2) - 32.27276861 * volts + 109.170896)
-
-
-def updateAll(self, canvas, mcp3208, serial, controller, rpm, speed, oilTemp, oilPressure, h2o, h2oEcu, battery, fuel,
-              throttle, clutch, brake, runTime, inj, duty, vtec, iat, ign, mapp, arrowLeft, arrowRight, accelerometer,
-              g, fuelText, gear):
-    rawSpeed = serial.get_vss()
-    speed.set_text(rawSpeed)
-    if (rawSpeed <= 0):
-        gear.set_text("N")
-    else:
-        gear.set_text(serial.get_gear())
-    rpm.set_value(serial.get_rpm() / 1000.0)
-    # mapp.setText(serial.getMap())
-    ign.update_value()
-    iat.update_value()
-    inj.update_value()
-    duty.update_value()
-    oilTemp.update_value()
-    oilPressure.update_value()
-    if self.fuelCounter < self.fuelCounterMax:
-        self.fuelAverage.append(self.adc2fuel(mcp3208.get_adc(3)))
-        self.fuelCounter = self.fuelCounter + 1
-    else:
-        self.fuelAverage = numpy.median(self.fuelAverage)
-        fuel.set_width(int(self.fuelAverage))
-        fuelText.set_text(int(self.fuelAverage))
-        self.fuelAverage = []
-        self.fuelCounter = 0
-    h2o.update_value()
-    # h2oEcu.setText(int(serial.getEct()))
-    battery.update_value()
-    throttle.set_height(serial.get_tps())
-    clutch.set_height(0)  # mcp3208.getADC(1))
-    # if(serial.getVtec()): vtec.setText("on")
-    # else: vtec.setText("off")
-    brake.set_height(0)  # mcp3208.getADC(7))'''
-    time = self.timer.get_time()
-    runTime.set_text(self.timer.getTimeString())
-    self.odometerText.set_text(locale.format("%d", self.odometer.getValue(serial.get_vss()), grouping=True))
-
-    '''axes = accelerometer.getAxes(True)
-	g.setGforce(axes['y'],axes['z'])
-	print axes['x'],axes['y'],axes['z']'''
-    canvas.after(10, controller.updateAll, canvas, mcp3208, serial, controller, rpm, speed, oilTemp, oilPressure, h2o,
-                 h2oEcu, battery, fuel, throttle, clutch, brake, runTime, inj, duty, vtec, iat, ign, mapp, arrowLeft,
-                 arrowRight, accelerometer, g, fuelText, gear)
+    def new_update_all(self, canvas, run_time):
+        canvas.gettags()
+        run_time.set_text(self.timer.getTimeString())
+        canvas.after(10, self.new_update_all, canvas, run_time)
--- main.py
+++ main.py
@@ -47,142 +47,14 @@ serial = CromeQD2()
 mcp3208 = MCP3208()
 controller = Controller()
 
-
-def adc2fuel(self, adc):
-    volts = (adc / 4096.000) * 4.80
-    return (int)(-7.348540077 * pow(10, -1) * pow(volts, 2) - 32.27276861 * volts + 109.170896)
-
-
-# conversion for VDO 323-057 sensor powered by 4.8v and read with a 56ohms voltage divider
-def adc2oiltemp(adc):
-    volts = (adc / 4096.000) * 4.80
-    return (int)(-9.805174198 * pow(10, -1) * pow(volts, 9) + 23.4368155 * pow(volts, 8) - 240.7430517 * pow(volts,
-                                                                                                             7) + 1390.11628 * pow(
-        volts, 6) - 4955.008229 * pow(volts, 5) + 11266.31187 * pow(volts, 4) - 16289.93484 * pow(volts,
-                                                                                                  3) + 14423.41426 * pow(
-        volts, 2) - 7152.975474 * volts + 1697.497838)
-
-
-def adc2oilpress(adc):
-    volts = (adc / 4096.000) * 4.80
-    return round(7.671035919 * pow(10, -2) * pow(volts, 7) - 1.077184901 * pow(volts, 6) + 6.295494139 * pow(volts,
-                                                                                                             5) - 19.62567902 * pow(
-        volts, 4) + 35.08161116 * pow(volts, 3) - 35.51613665 * pow(volts, 2) + 19.52857924 * volts - 4.551671147, 1)
-
-
-def oilTempSensor():
-    return adc2oiltemp(mcp3208.get_adc(7))
-
-
-def oilPressSensor():
-    return adc2oilpress(mcp3208.get_adc(5))
-
-
-def waterTempSensor():
-    return int(serial.get_ect())
-
-
-def batterySensor():
-    return round(serial.get_battery(), 1)
-
-
-def iatSensor():
-    return int(serial.get_iat())
-
-
-def injSensor():
-    return serial.get_inj()
-
-
-def ignSensor():
-    return int(serial.get_ign())
-
-
-def dutySensor():
-    return serial.get_duty_cycle()
-
-
 # init canvas
 canvas = Canvas(root, width=winWidth, height=winHeight, bg=Global.OFFBgColor)
 canvas.pack()
 
-wallpaper = Icon(canvas, "/home/pi/Desktop/HonDash/images/camo4.jpg", 1280 / 2, 800 / 2, 1280, 800, False)
-
-fuelIcon = Icon(canvas, "/home/pi/Desktop/HonDash/images/fuel.png", 352, 356, 45, 50, False)
-highBeamIcon = Icon(canvas, "/home/pi/Desktop/HonDash/images/lights.png", 900, 380, 63, 31, False)
-trunkIcon = Icon(canvas, "/home/pi/Desktop/HonDash/images/rear.png", 700, 380, 64, 38, False)
-oilIcon = Icon(canvas, "/home/pi/Desktop/HonDash/images/oil.png", 800, 380, 89, 21, False)
-
-# init graphics
-rpm = Gauge(canvas, 640, 260, 250, 135, 210, 1, 6, 9, Global.OFFgauge, 40, "Helvetica", "bold italic", 14, 4,
-            Global.OFFgauge, 30, 8, Global.OFFgauge, 5, 8, "red", "gray75", 50, Global.OFFneedleCoverColor)
-speed = Text(canvas, 135, 250, "Helvetica", speedFontSize, "bold italic", Global.OFFtextColor, "", "", "139")
-speedUnit = Text(canvas, 330, 290, "Helvetica", 30, "bold italic", Global.OFFtextColor, "", "", "km/h")
-mileage = 1  # Text(canvas,winWidth/2,(winHeight/10)*3,"Helvetica",10,"bold ","white","","","162.372 KM")
-fuel = Bar(canvas, 25, 380, 0, 280, 0, 50, 0, 100, Global.fuelColor, Global.OFFshadeColor)
-fuelText = Text(canvas, 175, 356, "Helvetica", 30, "bold italic", Global.fuelTextColor, "", "%", "100")
-
-# p=Text(canvas,640,50,"Helvetica",40,"bold italic","black","","7","")
-
-# pedals
-clutch = Bar(canvas, 1040, 798, 80, 80, 0, 380, 0, 200, Global.clutchColor, Global.OFFclutchBgColor)
-brake = Bar(canvas, 1119, 798, 80, 80, 0, 380, 2100, 2400, Global.brakeColor, Global.OFFbrakeBgColor)
-throttle = Bar(canvas, 1200, 798, 80, 80, 0, 380, 0, 100, Global.throttleColor, Global.OFFthrottleBgColor)
-
-# circles
-oilTemp = Circle(canvas, 150, 540, circleRadius, circleWidth, 240, 300, 0, 150, 80, 120, Global.circleMinColor,
-                 Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-                 "OIL T", Global.OFFshadeColor, oilTempSensor, None)
-
-oilPressure = Circle(canvas, 150, 715, circleRadius, circleWidth, 240, 300, 0, 10, 3, 6, Global.circleMinColor,
-                     Global.circleNormalColor, Global.circleNormalColor, circleValueSize, circleTextSize,
-                     Global.OFFtextColor, "OIL P", Global.OFFshadeColor, oilPressSensor, None)
-
-h2o = Circle(canvas, 400, 540, circleRadius, circleWidth, 240, 300, 0, 150, 80, 120, Global.circleMinColor,
-             Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-             "H2O T", Global.OFFshadeColor, waterTempSensor, None)
-
-g = None  # Gforce(canvas,450,680,205,2,2,"gray",6,"red",Global.OFFtextColor)
-
-battery = Circle(canvas, 650, 540, circleRadius, circleWidth, 240, 300, 0, 16, 11, 15, Global.circleMaxColor,
-                 Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-                 "BAT", Global.OFFshadeColor, batterySensor, None)
-
-iat = Circle(canvas, 900, 540, circleRadius, circleWidth, 240, 300, 0, 50, 0, 40, Global.circleMinColor,
-             Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-             "IAT", Global.OFFshadeColor, iatSensor, None)
-
-inj = Circle(canvas, 400, 715, circleRadius, circleWidth, 240, 300, 0, 20, 0, 20, Global.circleMinColor,
-             Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-             "INJ ms", Global.OFFshadeColor, injSensor, None)
-
-ign = Circle(canvas, 900, 715, circleRadius, circleWidth, 240, 300, 0, 60, 0, 60, Global.circleMinColor,
-             Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-             "IGN", Global.OFFshadeColor, ignSensor, None)
-
-duty = Circle(canvas, 650, 715, circleRadius, circleWidth, 240, 300, 0, 50, 0, 50, Global.circleMinColor,
-              Global.circleNormalColor, Global.circleMaxColor, circleValueSize, circleTextSize, Global.OFFtextColor,
-              "DTY %", Global.OFFshadeColor, dutySensor, None)
-
-# turn lights
-arrowLeft = Arrow(canvas, 100, 100, 0.35, Global.signalColor, "left", False)
-arrowRight = Arrow(canvas, 300, 100, 0.35, Global.signalColor, "right", False)
-
 # info
 runTime = Text(canvas, 100, 415, "Helvetica", 25, "bold italic", Global.OFFtextColor, "", "", "00:00:00")
-h2oEcu = None  # Text(canvas,910,324,"Helvetica",30,"bold italic",Global.OFFtextColor,"H2O: ","cº","44")
-vtec = None  # Text(canvas,700,594,"Helvetica",30,"bold italic",Global.OFFtextColor,"VTC: ","","off")
-mapp = None  # Text(canvas,702,738,"Helvetica",30,"bold italic",Global.OFFtextColor,"MAP: ","mBar","433")
-gear = Text(canvas, 1100, 220, "Helvetica", 350, "bold", Global.OFFtextColor, "", "", "N")
-gearUnit = None  # Text(canvas,745,385,"Helvetica",20,"bold italic",Global.OFFtextColor,"GEAR","","")
 
-odometer = Odometer()
-odometerText = Text(canvas, 300, 415, "Helvetica", 25, "bold italic", Global.OFFtextColor, "", " km", "")
-
-# update graphics
-canvas.after(10, controller.updateAll, canvas, mcp3208, serial, controller, rpm, speed, oilTemp, oilPressure, h2o,
-             h2oEcu, battery, fuel, throttle, clutch, brake, runTime, inj, duty, vtec, iat, ign, mapp, arrowLeft,
-             arrowRight, accelerometer, g, fuelText, gear)
+canvas.after(10, controller.new_update_all, canvas, runTime)
 
 # main loop
 root.mainloop()
