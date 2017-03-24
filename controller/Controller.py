cc7b191635cb6b2e26a3c99450896fa2e55e0417
pablobuenaposada <pablo.buenaposada@gmail.com>
2017-03-24 23:12:49 +0000

    new controller

--- controller/Controller.py
+++ controller/Controller.py
@@ -12,5 +12,6 @@ class Controller:
 
     def new_update_all(self, canvas, run_time):
         print canvas.type(canvas.find_all()[0])
+	print canvas.find_all()
         run_time.set_text(self.timer.time)
         canvas.after(10, self.new_update_all, canvas, run_time)
