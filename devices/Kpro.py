14f15816e24da626f829ec5e0e891d2ac178befc
pablobuenaposada <pablo.buenaposada@gmail.com>
2017-04-01 19:37:18 +0000

    aassd

--- devices/Kpro.py
+++ devices/Kpro.py
@@ -10,27 +10,38 @@ KPRO4_BAT = 4
 
 class Kpro:
     def __init__(self):
-        self.version = 2
-        self.data = []
-        self.dev = usb.core.find(idVendor=0x403, idProduct=0xf5f8)  # kpro2
-        # self.dev = usb.core.find(idVendor=0x1c40, idProduct=0x0434) #kpro4
+        self.data0 = []
+        self.data1 = []
+        self.data2 = []
+        self.dev = None
+        self.version = 0
+        if usb.core.find(idVendor=0x403, idProduct=0xf5f8) is not None: #kpro2
+            self.dev = usb.core.find(idVendor=0x403, idProduct=0xf5f8)
+            self.version = 2
+        elif usb.core.find(idVendor=0x1c40, idProduct=0x0434) is not None: #kpro4
+            self.dev = usb.core.find(idVendor=0x1c40, idProduct=0x0434)
+            self.version = 4
         if self.dev is not None:
-            self.dev.set_configuration()
-            cfg = self.dev.get_active_configuration()
-            intf = cfg[(0, 0)]
-            self.ep = usb.util.find_descriptor(
-                intf,
-                custom_match= \
-                    lambda e: \
-                        usb.util.endpoint_direction(e.bEndpointAddress) == \
-                        usb.util.ENDPOINT_OUT)
-
+            try:
+                self.dev.set_configuration()
+                cfg = self.dev.get_active_configuration()
+                intf = cfg[(0, 0)]
+                self.ep = usb.util.find_descriptor(
+                    intf,
+                    custom_match= \
+                        lambda e: \
+                            usb.util.endpoint_direction(e.bEndpointAddress) == \
+                            usb.util.ENDPOINT_OUT)
+            except:
+                pass
     def update(self):
         try:
             assert self.ep is not None
             self.ep.write('\x61')
-            # datos = dev.read(0x82,10000,1000) kpro4
-            self.data = self.dev.read(0x81, 10000, 1000)  # kpro2
+            if self.version == 2:
+                self.data1 = self.dev.read(0x81, 10000, 1000)  # kpro2
+            elif self.version == 4:
+                self.data1 = self.dev.read(0x82,10000,1000) #kpro4
         except:
             self.__init__()
             pass
@@ -38,8 +49,8 @@ class Kpro:
     def bat(self):
         try:
             if self.version == 2:
-                return self.data[KPRO2_BAT] * 0.1
+                return self.data1[KPRO2_BAT] * 0.1
             elif self.version == 4:
-                return self.data[KPRO4_BAT] * 0.1
+                return self.data1[KPRO4_BAT] * 0.1
         except:
             return 0
