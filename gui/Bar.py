f0b3f7c328b75da7bac1bba2f7ae94f3a861b3e6
pablobuenaposada <pablo.buenaposada@gmail.com>
2017-04-02 00:31:26 +0000

    aassd

--- gui/Bar.py
+++ gui/Bar.py
@@ -1,6 +1,6 @@
 class Bar:
-    def __init__(self, canvas, x, y, min_width, max_width, min_height, max_height, min_value, max_value, color,
-                 background_color):
+    def __init__(self, canvas, x, y, min_width, max_width, min_height, max_height, min_value, max_value, range_way, color,
+                 background_color, update_class=None, update_function=None, update_args=None):
         self.x = x
         self.y = y
         self.canvas = canvas
@@ -10,6 +10,10 @@ class Bar:
         self.maxHeight = -max_height
         self.minValue = min_value
         self.maxValue = max_value
+        self.update_class = update_class
+        self.update_function = update_function
+        self.update_args = update_args
+        self.range_way = range_way
         self.idBackgroundBar = self.canvas.create_rectangle(x, y, x + self.maxWidth, y + self.maxHeight,
                                                             fill=background_color, outline=background_color)
         self.idBar = self.canvas.create_rectangle(x, y, x + self.maxWidth, y + self.maxHeight, fill=color,
@@ -42,3 +46,15 @@ class Bar:
     def set_background_color(self, color):
         self.canvas.itemconfig(self.idBackgroundBar, fill=color)
         self.canvas.itemconfig(self.idBackgroundBar, outline=color)
+
+    def update(self):
+        if self.update_function is not None and self.update_args is None:
+            if self.range_way == 'height':
+                self.set_height(getattr(self.update_class, self.update_function)())
+            else:
+                self.set_width(getattr(self.update_class, self.update_function)())
+        if self.update_function is not None and self.update_args is not None:
+            if self.range_way == 'height':
+                self.set_height(getattr(self.update_class, self.update_function)(*self.update_args))
+            else:
+                self.set_width(getattr(self.update_class, self.update_function)(*self.update_args))
--- gui/Rpm.py
+++ gui/Rpm.py
@@ -1,9 +1,12 @@
 class Rpm:
     def __init__(self, canvas, x, y, width, height, bar_width, color, back_color, offset_degrees, total_degrees, min_value,
-                 max_value):
+                 max_value, update_class=None, update_function=None, update_args=None):
         self.canvas = canvas
         self.minValue = min_value
         self.maxValue = max_value
+        self.update_function = update_function
+        self.update_class = update_class
+        self.update_args = update_args
         self.totalDegrees = -total_degrees
         self.offsetDegrees = -180 - offset_degrees
         self.barWidth = bar_width
@@ -22,3 +25,9 @@ class Rpm:
 
         degrees = (((value_fill - self.minValue) * (self.totalDegrees - 0)) / (self.maxValue - self.minValue)) + 0
         self.canvas.itemconfig(self.idRpm, extent=degrees)
+
+    def update(self):
+        if self.update_function is not None and self.update_args is None:
+            self.set_rpm(getattr(self.update_class, self.update_function)())
+        if self.update_function is not None and self.update_args is not None:
+            self.set_rpm(getattr(self.update_class, self.update_function)(*self.update_args))
